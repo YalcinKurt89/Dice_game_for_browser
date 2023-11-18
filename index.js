783a27d82651c2b8840cb0690eacb6d94d07577c
YalcinKurt89 <96992131+YalcinKurt89@users.noreply.github.com>
2023-11-18 21:24:59 +0100

    Update index.js
    remove some spaces between the lines

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-
 var randomDiceImage = "dice" + randomNumber1 + ".png";
-
 var randomImageSource = "images/" + randomDiceImage;
-
 var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
 
-
-
-
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
 var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
-
 
 
 if (randomNumber1 > randomNumber2) {
